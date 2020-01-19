46a6f11dbd98d1eb6524419b5170f2d4dade5918
Brigette LeVert <bnl.levert@gmail.com>
2020-01-19 21:51:24 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count  #removed :genre to create an explicit genre method 
-  attr_reader :title :genre
+  attr_reader :title, :genre
   
   #add class constant in caps 
   #this class constant is an array 
