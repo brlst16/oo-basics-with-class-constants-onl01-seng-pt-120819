ec0582fe23ba32f903914850d2bdc349987ca23f
Brigette LeVert <bnl.levert@gmail.com>
2020-01-19 21:52:53 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count  #removed :genre to create an explicit genre method 
-  attr_reader :title, :genre
+  attr_reader :title, :genre, :brand 
   
   #add class constant in caps 
   #this class constant is an array 
@@ -13,6 +13,10 @@ class Book
   def genre=(genre)
     @genre = genre
     GENRES << genre   #connect class constant to the setter method by shoveling the method argument into the array
+  end 
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand 
   end 
 
   def turn_page
